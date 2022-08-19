bb95bc0d900de7c682aa179fe2c8c2e91792173c
Vova Laskarzhevskyy <83247078+voyager556321@users.noreply.github.com>
2022-08-19 13:11:12 +0300

    Add files via upload

---
+++ divisors.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n=0; 
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++) {
+		printf("%d: ",i);
+			
+		for(int j=2;j<=i;j++) {
+			if(i%j==0) {
+				printf("%d ",j);	
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
