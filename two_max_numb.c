442b368e7554c9dc45e3c80c21be9fc9b973be0b
Vova Laskarzhevskyy <83247078+voyager556321@users.noreply.github.com>
2022-08-19 13:12:27 +0300

    Add files via upload

---
+++ two_max_numb.c
@@ -0,0 +1,22 @@
+#include<stdio.h> 
+int main()
+{
+    int a[10]={2,45,3,34,5,6,29,12,9,18};
+    int i=0;
+    int max=a[0];
+    int second_max=a[0];
+    while(i<10) {
+        if(a[i]>max) {
+            max=a[i];
+        }
+        if(a[i]>second_max&&a[i]<max) {
+            second_max=a[i];
+        }
+        ++i;  
+    }
+    printf("%d %d",max,second_max);
+        return 0;
+    
+
+}
+
