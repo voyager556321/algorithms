ed516da6c901fb593728dfbf841228f8e40b6e66
voyager556321 <vovls5433@gmail.com>
2022-08-29 02:28:06 +0300

    Added new file area_triangle

--- divisors.c
+++ divisors.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-int main()
+
+int main(void)
 {
 	int n = 0; 
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++) {
 		printf("%d: ", i);
-			
 		for (int j = 2; j <= i;j++) {
 			if (i % j == 0) {
 				printf("%d ", j);	
--- two_max_numb.c
+++ two_max_numb.c
@@ -1,20 +1,21 @@
 #include<stdio.h> 
-int main()
+int main(void)
 {
-    int a[10]={2,45,3,34,5,6,29,12,9,18};
-    int i=0;
-    int max=a[0];
-    int second_max=a[0];
-    while(i<10) {
-        if(a[i]>max) {
-            max=a[i];
+        int a[10] = {2, 45, 3, 34, 5, 6, 29, 12, 9, 18};
+        int i = 0;
+        int max = a[0];
+        int second_max = a[0];
+        while(i < 10) {
+                if(a[i] > max) {
+                        max = a[i];
+
+                }
+                if((a[i] > second_max) && (a[i] < max)) {
+                        second_max = a[i];
+                }
+                ++i;
         }
-        if(a[i]>second_max&&a[i]<max) {
-            second_max=a[i];
-        }
-        ++i;  
-    }
-    printf("%d %d",max,second_max);
+        printf("%d %d", max, second_max);
         return 0;
     
 
