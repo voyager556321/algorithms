b073873fd40a0a261e02ac27dd326355bc4c4cc3
voyager556231 <vovls5433@gmail.com>
2023-02-19 00:12:09 +0200

    Correction style code

--- divisors.c
+++ divisors.c
@@ -6,7 +6,7 @@ int main(void)
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++) {
 		printf("%d: ", i);
-		for (int j = 2; j <= i;j++) {
+		for (int j = 2; j <= i; j++) {
 			if (i % j == 0) {
 				printf("%d ", j);	
 			}
