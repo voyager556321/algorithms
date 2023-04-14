0153b84521bfaccd889003f53c25ecc05a6eb042
voyager556231 <vovls5433@gmail.com>
2023-04-14 03:45:07 +0300

    The for loop was replaced by the fgets function

--- tasks/algorithm_luna.c
+++ tasks/algorithm_luna.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 int main(void)
 {
-        char arr[16];
+	const int size = 16;
+        char arr[size];
         int num = 0, sum = 0;
         printf("Enter the number card Visa: ");
-        for (int i = 0; i < 16; ++i){
-                scanf("%c", &arr[i]);
-        }
-        for (int i = 14; i >= 0; i -=2) {  
+
+	fgets(arr, size, stdin);
+
+        for (int i = size - 2; i >= 0; i -= 2) {  
                 num = (int)(arr[i] - '0');
                 num *= 2;
-                if (num  > 9) {
+                if (num > 9) {
                         num -= 9;
                 }
                 sum += num;
         }
-        for (int i = 15; i >= 0; i -=2) {
+
+        for (int i = size - 1; i >= 0; i -= 2) {
                 num = (int)(arr[i] - '0');
                 sum += num;
         }
-        if (*arr - '0' == 4 && sum % 10 == 0 && sizeof(arr) == 16) {
-                printf("Correct");
+        if ((arr[0] - '0' == 4)
+			&& (sum % 10 == 0)
+			&& sizeof(arr) == 16) {
+                printf("Correct\n");
         } else {
-                printf("Fake\n");
+                printf("Inauthentic\n");
         }
         return 0;
 }
-
-
